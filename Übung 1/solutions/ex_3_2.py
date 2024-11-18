b3b87f569ceb60cc6596613bcb67db9e7dc24602
Trevami <dimitrij94@yahoo.de>
2024-11-18 20:12:14 +0100

    code changes and new exercise 2 folder

--- Übung 1/solutions/ex_3_2.py
+++ Übung 1/solutions/ex_3_2.py
@@ -64,42 +64,43 @@ def calc_moon_to_earth(trajectories):
 
 
 if __name__ == "__main__":
-    data_path = Path(__file__).resolve().parent.parent / "files/solar_system.npz"
-    data = np.load(data_path)
-    names = data["names"]
-
-    x_init = data["x_init"]
-    v_init = data["v_init"]
-    m = data["m"]
-    g = data["g"]
-    # Transpose to seperate x and v vectors
     x = np.array(x_init).transpose()
     v = np.array(v_init).transpose()
 
-    # Calculate trajectories of planets
-    trajectories = run(x, v, 10e-2, 20, m, g, step_velocity_verlet).transpose()
+    # Calculate trajectories of planets and the distance beween earth and sun
+    # Calculate trajectories of planets and the distance beween earth and moon
+    timestep = 0.01
+    duration = 20
 
-    # # Trajectories plot
-    # for i in range(np.shape(trajectories)[1]):
-    #     plt.plot(
-    #         trajectories[0, i, :],
-    #         trajectories[1, i, :],
-    #         "-",
-    #         label=names[i].decode("UTF-8"),
-    #     )
-    # plt.xlabel(r"$x$ in au")
-    # plt.ylabel(r"$y$ in au")
-    # plt.legend(loc="lower right")
-    # plt.show()
+    traject_euler = ex_3_2.run(
+        x.copy(), v.copy(), timestep, duration, m, g, ex_3_1.step_euler).transpose()
+    dists_e_s_euler = get_dists_earth_sun(traject_euler)
+    dists_e_s_euler = get_dists_earth_moon(traject_euler)
 
-    # Trajectories plot
-    plt.plot(
-        trajectories[0, 2, :] - trajectories[0, 1, :],
-        trajectories[1, 2, :] - trajectories[1, 1, :],
-        "-",
-        label="moon compared to earth",
-    )
-    plt.xlabel(r"$x$ in au")
-    plt.ylabel(r"$y$ in au")
-    plt.legend(loc="lower right")
-    plt.savefig(plot_path / "Exc1_Plot_3_3_part2.png")
+    traject_sympl_euler = ex_3_2.run(
+        x.copy(), v.copy(), timestep, duration, m, g, ex_3_2.step_symplectic_euler).transpose()
+    dists_e_s_symp_euler = get_dists_earth_sun(traject_sympl_euler)
+    dists_e_s_symp_euler = get_dists_earth_moon(traject_sympl_euler)
+
+    traject_vv = ex_3_2.run(
+        x.copy(), v.copy(), timestep, duration, m, g, ex_3_2.step_velocity_verlet).transpose()
+    dists_e_s_vv = get_dists_earth_sun(traject_vv)
+    dists_e_s_vv = get_dists_earth_moon(traject_vv)
+
+    # Plot the distance of earth and sun for the 3 integrators
+    plot_path = Path(__file__).resolve().parent.parent/"plots"
+    # Plot the distance of earth and moon for the 3 integrators
+    timeline = np.linspace(0, duration, int(duration/timestep)+1)
+    plt.plot(timeline, dists_e_s_euler, '-',
+             label="Euler")
+    plt.plot(timeline, dists_e_s_symp_euler, '-',
+             label="Simplectic Euler")
+    plt.plot(timeline, dists_e_s_vv, '-',
+             label="Velocity Verlet")
+    plt.xlabel(r"$t$ in jears")
+    plt.ylabel(r"$\Delta r$ in au")
+    plt.xlabel(r"$t$ in years")
+    plt.ylabel(r"$\Delta r_{earh/moon}$ in au")
+    plt.legend(loc="upper left")
+    plt.show()
+    plt.savefig(plot_path/"Exc1_Plot_3_3_part1.png")
