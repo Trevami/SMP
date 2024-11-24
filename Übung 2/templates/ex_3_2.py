f8f7749eea656bbd3e495f9de7f0ee8739c237ad
Trevami <dimitrij94@yahoo.de>
2024-11-24 11:01:33 +0100

    changed ex_3_2 - 2

--- Übung 2/templates/ex_3_2.py
+++ Übung 2/templates/ex_3_2.py
@@ -8,16 +8,10 @@ def lj_potential(r_ij: np.ndarray) -> float:
     r_norm = np.linalg.norm(r_ij)
     return 4 * ((1 / r_norm) ** 12 - (1 / r_norm) ** 6)
 
-
-def lj_force(r_ij: np.ndarray) -> np.ndarray:
-    try:
-        return 48 / (r_ij ** 13) - 24 / (r_ij ** 7)
-    except (ZeroDivisionError):
-        return 0.0
     
-def lj_force1(r_ij: np.ndarray) -> np.ndarray:
+def lj_force(r_ij: np.ndarray) -> np.ndarray:
     r_norm = np.linalg.norm(r_ij)
-    return 48 * r_ij / (r_norm ** 13) - 24 * r_ij / (r_norm ** 7)
+    return -24 * r_ij * (r_norm ** 6 - 2) / r_norm ** 14
 
 
 def lj_pot_rep(r_ij: np.ndarray) -> float:
@@ -31,19 +25,17 @@ def lj_pot_rep(r_ij: np.ndarray) -> float:
 
 
 def lj_force_rep(r_ij: np.ndarray) -> np.ndarray:
-    try:
-        if np.linalg.norm(r_ij) <= 1.122462048309373:
-            return 48 / (r_ij ** 13) - 24 / (r_ij ** 7)
-        else:
-            return 0.0
-    except (ZeroDivisionError):
+    r_norm = np.linalg.norm(r_ij)
+    if np.linalg.norm(r_ij) <= 1.122462048309373:
+        return -24 * r_ij * (r_norm ** 6 - 2) / r_norm ** 14
+    else:
         return 0.0
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    num_vals = 10000
+    num_vals = 1000
     d_vec = np.array([np.linspace(0.85, 2.5, num_vals), np.zeros(num_vals)])
 
     # parameter signature="(n)->()" drops a dimension
