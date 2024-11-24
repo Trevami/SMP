efbe490b98ed8b851a0a2664c5acdac853aff5f8
Trevami <dimitrij94@yahoo.de>
2024-11-24 06:01:17 +0100

    changed ex_3_2

--- Übung 2/templates/ex_3_2.py
+++ Übung 2/templates/ex_3_2.py
@@ -5,27 +5,58 @@ import scipy.linalg
 
 
 def lj_potential(r_ij: np.ndarray) -> float:
-    print(r_ij)
-    return 4 * ((1 / np.linalg.norm(r_ij)) ** 12 - (1 / np.linalg.norm(r_ij)) ** 6)
+    r_norm = np.linalg.norm(r_ij)
+    return 4 * ((1 / r_norm) ** 12 - (1 / r_norm) ** 6)
 
 
 def lj_force(r_ij: np.ndarray) -> np.ndarray:
+    try:
+        return 48 / (r_ij ** 13) - 24 / (r_ij ** 7)
+    except (ZeroDivisionError):
+        return 0.0
+    
+def lj_force1(r_ij: np.ndarray) -> np.ndarray:
     r_norm = np.linalg.norm(r_ij)
-    x_comp = -4 * (12 * r_ij[0] / r_norm**14 - 6 * r_ij[0] / r_norm**8)
-    y_comp = -4 * (12 * r_ij[1] / r_norm**14 - 6 * r_ij[1] / r_norm**8)
-    return np.array([x_comp, y_comp])
+    return 48 * r_ij / (r_norm ** 13) - 24 * r_ij / (r_norm ** 7)
+
+
+def lj_pot_rep(r_ij: np.ndarray) -> float:
+    r_norm = np.linalg.norm(r_ij)
+    # value corresponds to r=2**(1/6) at minimum of LJ
+    if r_norm <= 1.122462048309373:
+        # schift by -epsilon (+1)
+        return 4 * ((1 / r_norm) ** 12 - (1 / r_norm) ** 6) + 1
+    else:
+        return 0.0
+
+
+def lj_force_rep(r_ij: np.ndarray) -> np.ndarray:
+    try:
+        if np.linalg.norm(r_ij) <= 1.122462048309373:
+            return 48 / (r_ij ** 13) - 24 / (r_ij ** 7)
+        else:
+            return 0.0
+    except (ZeroDivisionError):
+        return 0.0
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    d_vec = np.array([np.linspace(0.85, 2.5, 1), np.zeros(1)])
-    print(d_vec)
-    # d_vec = d_vec.transpose()
-    # print(np.shape(d_vec))
-    # print(d_vec[0:10])
-    vfunc_lj = np.vectorize(lj_potential)
-    # vfunc_force = np.vectorize(lj_force)
-    lj_vec = vfunc_lj(d_vec)
-    # lj_force = vfunc_force(d_vec)
-    # plt.plot(lj_vec)
+    num_vals = 10000
+    d_vec = np.array([np.linspace(0.85, 2.5, num_vals), np.zeros(num_vals)])
+
+    # parameter signature="(n)->()" drops a dimension
+    lj_pot_vec_func = np.vectorize(lj_potential, signature="(n)->()")
+    lj_force_vec_func = np.vectorize(lj_force)
+
+    lj_pot_energies = lj_pot_vec_func(d_vec.T)
+    lj_forces = lj_force_vec_func(d_vec)
+
+    plt.axhline(0, color="gray", linewidth=1,
+                linestyle="--")  # Horizontal line at y=0
+    plt.plot(d_vec[0], lj_pot_energies)
+    plt.plot(d_vec[0], lj_forces[0])
+    plt.ylim(-2.5, 1)
+    plt.xlim(0.5,2.5)
+    plt.show()
